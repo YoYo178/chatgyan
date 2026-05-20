07c3ad798cc3317d35530f3d5f88ee67c7b2156f
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-05-20 21:05:23 +0530

    fix(backend): update express type augmentation

--- backend/src/@types/express/index.d.ts
+++ backend/src/@types/express/index.d.ts
@@ -1,9 +1,15 @@
-declare namespace Express {
-    interface Request {
-        user: {
-            id: string,
-            username: string,
-            email: string,
-        };
+import { ChatGyanSocketServer } from '@src/types/socket.types.ts';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user: {
+                id: string,
+                username: string,
+                email: string,
+            };
+            io: ChatGyanSocketServer;
+
+        }
     }
 }
