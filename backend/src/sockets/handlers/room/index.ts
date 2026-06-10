a9868599ae48a574cb1443d4f0b7b93cacab4bd3
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-06-10 22:24:01 +0530

    feat(backend): add join and leave events

--- backend/src/sockets/handlers/room/index.ts
+++ backend/src/sockets/handlers/room/index.ts
@@ -1,10 +1,14 @@
 import type { ChatGyanSocket, ChatGyanSocketServer } from '@src/types/socket.types.js';
 
 import { getCreateRoomEventCallback } from './createRoom.js';
+import { getJoinRoomEventCallback } from './joinRoom.js';
+import { getLeaveRoomEventCallback } from './leaveRoom.js';
 
 export function registerRoomHandlers(
   io: ChatGyanSocketServer,
   socket: ChatGyanSocket,
 ) {
   socket.on('createRoom', getCreateRoomEventCallback(io, socket));
+  socket.on('joinRoom', getJoinRoomEventCallback(io, socket));
+  socket.on('leaveRoom', getLeaveRoomEventCallback(io, socket));
 }
