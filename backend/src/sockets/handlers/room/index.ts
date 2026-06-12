e8017b125b0633f6940702157ed4fe582691f126
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-06-12 22:40:20 +0530

    feat(backend): add delete and update room events

--- backend/src/sockets/handlers/room/index.ts
+++ backend/src/sockets/handlers/room/index.ts
@@ -1,8 +1,13 @@
-import type { ChatGyanSocket, ChatGyanSocketServer } from '@src/types/socket.types.js';
+import type {
+  ChatGyanSocket,
+  ChatGyanSocketServer,
+} from '@src/types/socket.types.js';
 
 import { getCreateRoomEventCallback } from './createRoom.js';
+import { getDeleteRoomEventCallback } from './deleteRoom.js';
 import { getJoinRoomEventCallback } from './joinRoom.js';
 import { getLeaveRoomEventCallback } from './leaveRoom.js';
+import { getUpdateRoomEventCallback } from './updateRoom.js';
 
 export function registerRoomHandlers(
   io: ChatGyanSocketServer,
@@ -11,4 +16,6 @@ export function registerRoomHandlers(
   socket.on('createRoom', getCreateRoomEventCallback(io, socket));
   socket.on('joinRoom', getJoinRoomEventCallback(io, socket));
   socket.on('leaveRoom', getLeaveRoomEventCallback(io, socket));
+  socket.on('updateRoom', getUpdateRoomEventCallback(io, socket));
+  socket.on('deleteRoom', getDeleteRoomEventCallback(io, socket));
 }
